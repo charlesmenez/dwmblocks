f1229dce8307471184e108d843dae8bdf5c66e6f
charlesmenez <menezessscharles@gmail.com>
2022-12-21 15:45:12 -0300

    minor changes

--- config.h
+++ config.h
@@ -5,22 +5,9 @@ static const Block blocks[] = {
     /* {"⌨", "sb-kbselect", 0, 30}, */
     {"", "cat /tmp/recordingicon 2>/dev/null", 0, 9},
     {"", "sb-music", 0, 11},
-    /* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,
-     * 22},
-     */
-    /* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
-    /* {"",	"sb-price link \"Chainlink\" 🔗",			300,
-     * 25},
-     */
-    /* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
-    /* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
-    /* {"",	"sb-price btc Bitcoin 💰",				9000,
-     * 21},
-     */
     {"", "sb-torrent", 20, 7},
     {"", "sb-memory", 10, 14},
     {"", "sb-cpu", 10, 18},
-    /* {"",	"sb-moonphase",	18000,	17}, */
     {"", "sb-nettraf", 1, 16},
     {"", "sb-clock", 60, 1},
 };
