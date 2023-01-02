0c6f595786071a3d5d59afeb2801d3580b6a4a10
charlesmenez <menezessscharles@gmail.com>
2023-01-02 14:31:09 -0300

    update

--- config.h
+++ config.h
@@ -3,9 +3,6 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     /* {"⌨", "sb-kbselect", 0, 30}, */
-    {"", "cat /tmp/recordingicon 2>/dev/null", 0, 9},
-    {"", "sb-music", 0, 11},
-    {"", "sb-torrent", 20, 7},
     {"", "sb-memory", 10, 14},
     {"", "sb-cpu", 10, 18},
     {"", "sb-nettraf", 1, 16},
